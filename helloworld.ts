ff8be9ccb9c2a35fa36eb422ec1ac4d8c13e2f02
Jiani Liu <jianiliu@Jianis-MacBook-Air.local>
2022-03-19 23:22:08 -0400

    declare variables

---
+++ helloworld.ts
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+// declare variables with a let
+let num:Number = 5
+console.log(num)
+num = 6
+console.log(num)
+
+
+// declare variables with a const
+const myBoolean:boolean = true
+console.log(myBoolean)
