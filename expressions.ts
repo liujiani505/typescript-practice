2d1bfcdfb301ff657b38ac609ee336234980bbc9
Jiani Liu <jianiliu@Jianis-MacBook-Air.local>
2022-03-20 22:14:53 -0400

    expressions

---
+++ expressions.ts
@@ -0,0 +1,39 @@
+let num1:number
+const five:number = 5
+
+// math expressions
+
+num1 = five + five - five * five / five
+
+console.log(num1)
+
+num1 = 5 + (5 - 5 * 5) / 5
+
+console.log(num1)
+
+num1 = 10 % 2 // modulo/modulus
+
+
+// string expressions
+
+let myString:string
+let first:string = "five"
+let second:string = "cheese"
+//concatenation
+myString = "five" + "cheese"
+
+//interpolation
+myString = `${first}${second}`
+
+console.log(myString)
+
+
+// boolean expressions
+
+let myBoolean1:boolean
+
+myBoolean1 = 5 > 6
+console.log(myBoolean1)
+
+myBoolean1 = 5 <= 6
+console.log(myBoolean1)
--- helloworld.ts
+++ helloworld.ts
@@ -1,7 +1,7 @@
 console.log("Hello World")
 
 // declare variables with a let
-let num:Number = 5
+let num:number = 5
 console.log(num)
 num = 6
 console.log(num)
@@ -10,3 +10,4 @@ console.log(num)
 // declare variables with a const
 const myBoolean:boolean = true
 console.log(myBoolean)
+
