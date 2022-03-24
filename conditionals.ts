3301ca2ab0699840ddf0c14e64fe3fd7f525cb37
Jiani Liu <jianiliu@Jianis-MacBook-Air.local>
2022-03-24 00:02:51 -0400

    switch/ternary

--- conditionals.ts
+++ conditionals.ts
@@ -1,10 +1,14 @@
 // if statement
 
-let cheese
-const value:string = "cheap"
+let cheese:string
+const value:string = "medium"
 
 if(value === "expensive") {
     cheese = "manchego"
+} else if (value === "medium"){
+    cheese = "gouda"
+} else {
+    cheese = "American"
 }
 
 console.log(cheese)
@@ -14,3 +18,24 @@ console.log(cheese)
 if(!cheese) {
     console.log("You have no cheese")
 }
+
+// Switch statement
+let cheese2:string
+
+switch(value) {
+    case "expensive":
+        cheese2 = "manchego"
+        break
+    case "medium":
+        cheese2 = "gouda"
+        break
+    default:
+        cheese2 = "American"
+}
+
+console.log(cheese2)
+
+// ternary operator (if as an expression)
+let cheese3:string
+cheese3 = value === "expensive" ? "manchego" : value === "medium" ? "gouda" : "American"
+console.log(cheese3)
