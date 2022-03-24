da98e77b0fdd29db53377bddabc85e1f42504219
Jiani Liu <jianiliu@Jianis-MacBook-Air.local>
2022-03-23 23:17:03 -0400

    if statement

---
+++ conditionals.ts
@@ -0,0 +1,16 @@
+// if statement
+
+let cheese
+const value:string = "cheap"
+
+if(value === "expensive") {
+    cheese = "manchego"
+}
+
+console.log(cheese)
+
+// checking if a variable has a value
+// everything is true expect false, 0, "", undefined, null, NaN
+if(!cheese) {
+    console.log("You have no cheese")
+}
