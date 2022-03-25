c4edd96c9655c9f2f79ee953f322863186b209d4
Jiani Liu <jianiliu@Jianis-MacBook-Air.local>
2022-03-25 09:44:51 -0400

    arrayargs

---
+++ arrayargs.ts
@@ -0,0 +1,8 @@
+const myArray: Array<number> = [1,2,3,4,5]
+
+console.log(myArray)
+console.log(myArray[0])
+
+console.log("------ Result of the Addition ------")
+console.log(parseInt(Deno.args[0]) + parseInt(Deno.args[1]))
+
